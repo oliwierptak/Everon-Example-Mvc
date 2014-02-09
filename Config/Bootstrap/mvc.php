10c7a0f04e453522d12e1f821c2fa8fc352b2588
oliwierptak <oliwierptak@gmail.com>
2014-02-09 22:19:34 +0100

    updated test

--- Config/Bootstrap/mvc.php
+++ Config/Bootstrap/mvc.php
@@ -3,7 +3,7 @@ namespace Everon;
 
 $nesting = implode('..', array_fill(0, 3, DIRECTORY_SEPARATOR));
 $EVERON_ROOT =  realpath(dirname(__FILE__).$nesting).DIRECTORY_SEPARATOR;
-$EVERON_SOURCE_ROOT = implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), '..', '..', 'vendor', 'everon', 'src', 'Everon']).DIRECTORY_SEPARATOR;
+$EVERON_SOURCE_ROOT = implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), '..', '..', 'vendor', 'everon', 'everon', 'src', 'Everon']).DIRECTORY_SEPARATOR;
 
 require_once(
     implode(DIRECTORY_SEPARATOR,

b9661d0fed8dbb6f665209e4665426bb795006a7
oliwierptak <oliwierptak@gmail.com>
2014-02-10 17:20:13 +0100

    modules for sample application

---
+++ Module/UserManagement/Controller/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Everon\Module\UserManagement\Controller;
+
+use Everon\Dependency;
+use Everon\Interfaces;
+use Everon\Mvc\Controller as MvcController;
+
+class Admin extends MvcController implements Interfaces\Controller
+{
+
+}
---
+++ Module/UserManagement/View/Admin.php
@@ -0,0 +1,11 @@
+<?php
+namespace Everon\Module\UserManagement\View;
+
+use Everon\View;
+use Everon\Dependency;
+use Everon\View as DefaultView;
+
+class Admin extends DefaultView
+{
+
+}

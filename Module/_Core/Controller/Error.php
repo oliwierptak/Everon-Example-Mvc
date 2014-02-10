9956c7fc4469a51e1a1c5bacf43d1e0391168770
oliwierptak <oliwierptak@gmail.com>
2014-02-10 23:10:23 +0100

    - added default _Core module - removed default router.ini, its content will be combined out of route.ini files under Modules

---
+++ Module/_Core/Controller/Error.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Everon\Module\UserLogin\Controller;
+
+use Everon\Dependency;
+use Everon\Interfaces;
+use Everon\Mvc\Controller as MvcController;
+
+class Error extends MvcController implements Interfaces\Controller
+{
+    public function show()
+    {
+    }
+}

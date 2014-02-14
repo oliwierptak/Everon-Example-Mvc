743c5cb94abef2f540ada8bf284712d9b8a2afd7
oliwierptak <oliwierptak@gmail.com>
2014-02-14 10:15:21 +0100

    cleanups, tests updates

--- Config/Bootstrap/mvc.php
+++ Config/Bootstrap/mvc.php
@@ -58,8 +58,8 @@ $Container->register('ViewManager', function() use ($Factory) {
 });
 
 
-//cleanup global state after bootstrap, otherwise phpunit will complain
-unset($CustomSetup);
+//cleanup global state after bootstrap, otherwise phpunit will complain, and $backupGlobalsBlacklist does not work
+unset($CustomExceptionHandler);
 unset($nesting);
 unset($Factory);
 unset($Container);
